3fa16ed1a812a6fd5442179c082160246110da19
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-08-09 15:38:32 +0200

    filter black-listed nodes and interfaces

--- rosgraph_monitor/graph.py
+++ rosgraph_monitor/graph.py
@@ -5,9 +5,9 @@ from re import compile
 
 
 BLACK_LIST_PARAM = ['/rosdistro', '/rosversion', '/run_id']
-BLACK_LIST_TOPIC = ["/tf", "/tf_static", "/rosout", "/clock"]
-BLACK_LIST_SERV = ["/set_logger_level", "/get_loggers"]
-BLACK_LIST_NODE = ["/rosout"]
+BLACK_LIST_TOPIC = ["/tf", "/tf_static", "/rosout", "/clock", "/parameter_events", "/diagnostics"]
+BLACK_LIST_SERV = ["describe_parameters", "get_parameter_types", "get_parameters", "list_parameters", "set_parameters", "set_parameters_atomically"]
+BLACK_LIST_NODE = ["rosout", "_ros2cli_daemon_0"]
 
 ACTION_FILTER = ['goal', 'cancel']
 ACTION_FILTER2 = ['status', 'result', 'feedback']
@@ -35,21 +35,27 @@ def create_ros_graph_snapshot(graph_node: Node):
     nodes_ns = graph_node.get_node_names_and_namespaces()
 
     for node in nodes_ns:
+        if node[0] in BLACK_LIST_NODE or node[0] == graph_node.get_name():
+            continue
+
         if node not in components:
             node_name = node[1] + node[0]
             _init_node_dict(components, node_name)
 
         pubs = graph_node.get_publisher_names_and_types_by_node(node[0], node[1])
         for pub in pubs:
-            components[node_name]['publishers'][pub[0]] = pub[1][0]
+            if not pub[0].endswith(tuple(BLACK_LIST_TOPIC)):
+                components[node_name]['publishers'][pub[0]] = pub[1][0]
 
         subs = graph_node.get_subscriber_names_and_types_by_node(node[0], node[1])
         for sub in subs:
-            components[node_name]['subscribers'][sub[0]] = sub[1][0]
+            if not sub[0].endswith(tuple(BLACK_LIST_TOPIC)):
+                components[node_name]['subscribers'][sub[0]] = sub[1][0]
 
         srvs = graph_node.get_service_names_and_types_by_node(node[0], node[1])
         for srv in srvs:
-            components[node_name]['service_servers'][srv[0]] = srv[1][0]
+            if not srv[0].endswith(tuple(BLACK_LIST_SERV)):
+                components[node_name]['service_servers'][srv[0]] = srv[1][0]
 
         parameters = _get_parameter_names_by_node(graph_node, node_name)
         components[node_name]['parameters'] = parameters
